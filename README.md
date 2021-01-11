16a6ec2746cafbbb3a1cb81802fa78f5b45faa76
nefiertsrebliS <silberstreifen@freenet.de>
2021-01-11 18:30:22 +0100

    V1.00 Baseversion

---
+++ README.md
@@ -0,0 +1,128 @@
+# MQTT-Landroid-Bridge
+
+## Worx (Kress and Landxcape) adapter for IP-Symcon
+
+IP-Symcon-Mowercontrol via MQTT and cloud
+
+This adapter connects IP-Symcon with your Worx-, Kress- or Landxcape-mower via Cloud. 
+This bridge is powered by the [ioBroker.worx-Adapter](https://github.com/iobroker-community-adapters/ioBroker.worx) from the ioBroker-Community. Thanks to meisterTR and all the people, who build this great adapter.
+
+## Requisites
+
+To use this bridge, you need [IP-Symcon V5.0 or newer](https://www.symcon.de/) and the newest Version of [Node.js](https://nodejs.org/).
+
+## Setup
+
+1. Make sure you have [Node.js](https://nodejs.org) installed (tested with Node.js v12).
+1. Check out the source code and build it:
+    ```
+    git clone https://github.com/nefiertsrebliS/mqtt-landroid-bridge.git
+    cd mqtt-landroid-bridge
+    npm install
+    ```
+1. Update ```config.json``` to match your environment.
+1. Run the bridge:
+    ```
+    node bridge.js
+    ```
+1. Optional (Linux only): 
+    1. Set up an init.d script to start the bridge on system startup (see example in initd-script folder).
+    1. Set up a systemctl script to start the bridge on system startup (see example in systemctl-script folder).
+
+## Configuration
+
+Please modify your config.json like this:
+
+```
+{
+	"cloud": {
+	    "email": "yourmail@mailserver.org",
+	    "pwd": "TopSecret!",
+	    "type": "worx"
+	},
+	"mqtt": {
+	    "url": "mqtt://yourIPS-Server"
+	},
+	"mower": [
+		{
+		    "sn": "serialNumberOfYourMower",
+		    "topic": "yourbestmower"
+		}
+	],
+    "logLevel": "info"
+}
+```
+were
+* email and pwd are your login-data of your Cloud.
+* type is the type of your Mower and Cloud. It may be worx, landxcape or kress.
+* sn is the Serialnumber of your Mower. You may find it on your mower or on startup of this bridge in LogLevel "debug".
+* topic is a MQTT-topic of your choice to subscribe to on your IP-Symcon-MQTT-Server. Make sure, that your IP-Symcon MQTTworxGateway uses the same topic.
+* for logLevel you have the options debug, info, warn, error and silent.
+
+## Managing multiple mowers in the same Cloud
+
+If you have more than one mower connected to your Cloud Account, please modify your config.json like this:
+```
+{
+	"cloud": {
+	    "email": "yourmail@mailserver.org",
+	    "pwd": "TopSecret!",
+	    "type": "kress"
+	},
+	"mqtt": {
+	    "url": "mqtt://yourIPS-Server"
+	},
+	"mower": [
+		{
+		    "sn": "serialNumberOfYourMower",
+		    "topic": "yourbestmower"
+		},
+		{
+		    "sn": "serialNumberOfYourSecondMower",
+		    "topic": "yourOthermower"
+		}
+	],
+    "logLevel": "debug"
+}
+```
+## Managing mowers in different Clouds
+
+If your mowers ar in different clouds, please run seperat instances of this bridge for each cloud.
+
+## Recommendation for the winter break
+
+If your mower is offline for a longer period, please disable the MQTT-Landroid-Bridge.
+
+## Connecting to IP-Symcon
+
+To connect this Landroid Bridge to [IP-Symcon](https://www.symcon.de/), add the newest Version of the [MQTTworx-Modul](https://github.com/nefiertsrebliS/MQTTworx) to your Symcon-installation after Landroid Bridge is up and running successfully (see above).
+
+## Changelog
+
+| Version | Changes								|
+| --------|-------------------------------------|
+| V1.00   | Baseversion							|
+
+## License
+
+MIT License
+
+Copyright (c) 2021 nefiertsrebliS
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
---
+++ bridge.js
@@ -0,0 +1,156 @@
+// --------------------------------------------------------------------------------------------
+// MQTT-Landroid-Bridge for Node.js
+// version 1.0.0
+// --------------------------------------------------------------------------------------------	
+
+	"use strict";
+	const mqtt = require('mqtt');
+	const worx = require('iobroker.worx/lib/api');
+	const config = require('./config.json');
+	const logLevel = ['debug', 'info', 'warn', 'error', 'silent'];
+	var client;
+	var clientStatus = false;
+	var onlineStatus = {};
+	var data;
+	var mowerId;
+	var worxCloud;
+
+	var adapter = {
+		config:{
+			server:"worx"
+		},
+		log: {
+			debug: function (msg) {
+				if(config.logLevel < 1) console.log(adapter.getTimestamp()+' DEBUG: ' + msg);
+			},
+			info: function (msg) {
+				if(config.logLevel < 2) console.log(adapter.getTimestamp()+' INFO: ' + msg);
+			},
+			warn: function (msg) {
+				if(config.logLevel < 3) console.log(adapter.getTimestamp()+' WARN: ' + msg);
+			},
+			error: function (msg) {
+				if(config.logLevel < 4) console.log(adapter.getTimestamp()+' ERROR: ' + msg);
+			}
+		},
+		msg: {
+			info: [],
+			error: [],
+			debug: [],
+			warn: []
+		},
+		// Build Timestamp
+		getTimestamp : function() {
+			var date = new Date();
+			return date.toUTCString();
+		}
+	};
+	
+	//Get logLevel
+	if(typeof config.logLevel == "undefined") config.logLevel = "";
+	config.logLevel = logLevel.indexOf(config.logLevel.toLowerCase());
+	if(config.logLevel < 0) {
+		config.logLevel = 0;
+	    adapter.log.error('logLevel is undefined! Your options are "info", "debug", "warn", "error", "silent". logLevel "info" is set');
+	}
+	// Establish MQTT Bridge
+	main();
+
+// --------------------------------------------------------------------------------------------	
+
+	// set OnlineStatus
+	function setOnlineStatus(sn, online) {
+		if(onlineStatus[sn] != online){
+			onlineStatus[sn] = online;
+			config.mower.forEach(function(device) {
+				if(device.sn == sn){
+					adapter.log.info('Mower ('+sn+') with Topic "'+device.topic+'" is '+(online?'online':'offline'));
+					if(onlineStatus[sn] && clientStatus) adapter.log.info('Bridge for Mower ('+sn+') sucessfully established');
+				}
+			});
+		}
+		if(clientStatus)config.mower.forEach(function(device) {
+			if(device.sn == sn)	client.publish(device.topic+'/', '{"online":'+online+'}');
+		});
+	}
+	
+	function main() {
+		// MQTT-Connection to local Server
+		client  = mqtt.connect(config.mqtt.url);
+		client.on('connect', function () {
+			config.mower.forEach(function(device) {
+				client.subscribe(device.topic+'/set/json', function (err) {
+					if (!err) {
+						adapter.log.info('Topic '+device.topic+' sucessfully connected with local MQTT-Server');
+						clientStatus = true;
+						var online = false;
+						for (const key in onlineStatus) {
+							if(onlineStatus[key]) online = true;
+						}
+						if(online) adapter.log.info('Bridge sucessfully established');
+					}
+				});
+			});
+		})
+		 
+		client.on('close', function() {
+			if(clientStatus) adapter.log.info('Connection with local MQTT-Server closed');
+			clientStatus = false;
+		});
+		 
+		client.on('message', function (topic, message) {
+		  // message is Buffer
+		    adapter.log.info('Message received from '+topic+' - '+message.toString());
+			config.mower.forEach(function(device) {
+				if(device.topic+'/set/json' == topic){
+					if(onlineStatus[device.sn]){
+						adapter.log.info('Forwarding to Mower ('+device.sn+')');
+						worxCloud.sendMessage(message.toString(), device.sn);
+					}else{
+						adapter.log.info('Forwarding rejected, Mower ('+device.sn+') is offline');
+						setOnlineStatus(device.sn, onlineStatus[device.sn]);
+					}
+				}
+			});
+		})
+
+		// MQTT-Connection to Cloud-Server
+		worxCloud = new worx(config.cloud.email, config.cloud.pwd, adapter);
+        worxCloud.on('connect', worxc => {
+		    adapter.log.info('sucessfully connected with '+config.cloud.type+'Cloud!');
+        });
+
+        worxCloud.on('found', function (mower) {
+			config.mower.forEach(function(device) {
+				if(device.sn == mower.serial){
+					adapter.log.info('sucessfully connected with Mower ('+mower.serial+')');
+					setOnlineStatus(mower.serial, mower.online);
+				}
+			});
+        });
+
+        worxCloud.on('online', function (mower) {
+			setOnlineStatus(mower.serial, mower.online);
+        });
+
+        worxCloud.on('offline', function (mower) {
+			setOnlineStatus(mower.serial, mower.online);
+        });
+
+	    worxCloud.on('mqtt', (mower, mower_data) => {
+
+			setOnlineStatus(mower.serial, true);
+			//Send data to local MQTT-Server
+			config.mower.forEach(function(device) {
+				if(device.sn == mower.serial) client.publish(device.topic+'/', JSON.stringify(mower_data));
+			});
+        });
+
+        worxCloud.on('error', err => {
+		    adapter.log.error(err);
+		    if(err.statusCode == 401){
+	            process.exit();
+			}
+        });
+	}
+
